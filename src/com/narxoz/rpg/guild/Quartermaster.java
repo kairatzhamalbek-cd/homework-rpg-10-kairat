f997a9971f1ba75c2693a6c25a79e00dd46b622f
kairatzhamalbek-cd <kairat.zhamalbek@narxoz.kz>
2026-05-05 10:52:52 +0600

    Add Quartermaster class for supplies and rewards coordination

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -1,8 +1,5 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for gear, supplies, and rewards.
- */
 public class Quartermaster extends GuildMember {
 
     public Quartermaster(String name, GuildMediator mediator) {
@@ -10,12 +7,19 @@ public class Quartermaster extends GuildMember {
     }
 
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
-    }
+        System.out.println("[Quartermaster " + getName() + "] received from "
+                + from.getName() + " | topic: " + topic + " | " + payload);
+
+        if (topic.equals("orders")) {
+            System.out.println("[Quartermaster " + getName() + "] preparing supplies.");
+        } else if (topic.equals("urgent")) {
+            System.out.println("[Quartermaster " + getName() + "] rushing equipment!");
+        } else if (topic.equals("rewards")) {
+            System.out.println("[Quartermaster " + getName() + "] calculating rewards.");
+        }    }
 }
